cfdad8633b96fed0e9f0516dd6de17de3779f8f3
Owner <michalalhazov12@gmail.com>
2026-06-01 09:05:14 +0300

    add

--- src/RenderConfig.java
+++ src/RenderConfig.java
@@ -1,2 +1,20 @@
 public class RenderConfig {
+
+    class RenderConfig {
+        public String wallCellColor;
+        public String pathColor;
+        public boolean drawGrid;
+        public String gridColor;
+        public int animationDelayMs;
+
+        public static RenderConfig fromJson(String json) {
+            RenderConfig config = new RenderConfig();
+            config.wallCellColor = extractJsonValue(json, "wallCellColor");
+            config.pathColor = extractJsonValue(json, "pathColor");
+            config.drawGrid = Boolean.parseBoolean(extractJsonValue(json, "drawGrid"));
+            config.gridColor = extractJsonValue(json, "gridColor");
+            config.animationDelayMs = Integer.parseInt(extractJsonValue(json, "animationDelayMs"));
+            return config;
+        }
+}
 }
